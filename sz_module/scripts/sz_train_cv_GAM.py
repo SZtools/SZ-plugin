fe294087a6a9f6d7bfccda9fd37955c179726f9a
giacomo <giacomotitti@gmail.com>
2026-08-13 15:19:14 +0200

    Return declared GAM training outputs

--- sz_module/scripts/sz_train_cv_GAM.py
+++ sz_module/scripts/sz_train_cv_GAM.py
@@ -282,7 +282,8 @@ class CoreAlgorithmGAM_cv():
             }
             outputs['error_train']=SZ_utils.stamp_qq_fit(alg_params)
 
-        results['out'] = parameters['out']
+        results[self.OUTPUT] = parameters['out']
+        results[self.OUTPUT3] = parameters['folder']
 
         feedback.setCurrentStep(5)
         if feedback.isCanceled():
